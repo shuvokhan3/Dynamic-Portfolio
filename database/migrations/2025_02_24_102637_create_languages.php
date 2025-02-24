98e5723c4eee9f39fd8d75ea02f19301529ac17f
shuvokhan3 <shuvoj360@gmail.com>
2025-02-24 23:42:10 +0600

    Create migration file and add table

--- database/migrations/2025_02_24_102637_create_languages.php
+++ database/migrations/2025_02_24_102637_create_languages.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('languages');
     }
 };
